2098a6bae691cffb867229451452a968bc921160
박종훈 <m05214@marketboro.com>
2023-09-08 11:24:46 +0900

    Add #8 SocialLoginUserDto
    SocialLoginUserDto 소셜로그인 DTO 추가함

---
+++ src/main/java/org/awesomeboro/awesome_bro/dto/user/SocialLoginUserDto.java
@@ -0,0 +1,19 @@
+package org.awesomeboro.awesome_bro.dto.user;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class SocialLoginUserDto {
+	private String name;
+	private String email;
+	private String nickname;
+	private String loginType;
+	private String socialId;
+	private String profilePicture;
+
+}
