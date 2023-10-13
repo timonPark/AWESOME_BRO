fa9b9174da435db2d61a3b2ef000d64c533f5f53
이관영 <71133204+stufit@users.noreply.github.com>
2023-10-13 17:11:59 +0900

    Add #36 게시판 crud

--- src/main/java/org/awesomeboro/awesome_bro/article/ArticleRepository.java
+++ src/main/java/org/awesomeboro/awesome_bro/article/ArticleRepository.java
@@ -1,6 +1,12 @@
 package org.awesomeboro.awesome_bro.article;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@Transactional
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    List<Article> findAllByUseYn(String useYn, Sort sort);
 }
